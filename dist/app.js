3bd2ce2cf3e1a2bc2a2f3cf6ede0858e0c801ea7
saad <shksaad11@gmail.com>
2021-01-11 16:27:53 +0530

    commit 1.1

---
+++ dist/app.js
@@ -0,0 +1,73 @@
+const USERAPI = "https://api.github.com/users/";
+
+
+const form = document.getElementById("form");
+const inputBox = document.getElementById("input-box");
+// const searchBtn = document.getElementById("search");
+getUser("saad696")
+async function getUser(username){
+    const response = await fetch(USERAPI + username);
+    const responseData = await response.json();
+
+    createUserCard(responseData);
+    getRepository(username);
+}
+
+async function getRepository(username){
+    const response = await fetch(USERAPI + username + "/repos");
+    const responseData = await response.json();
+
+    addReposToCard(responseData);
+    console.log(responseData)
+}
+
+const createUserCard = (user) => {
+    const cardHTML = 
+    `
+    
+    <div class="card">
+        <div class="avatar-container">
+            <img src="${user.avatar_url}" alt="${user.name}" class="avatar animate__animated animate__pulse animate__infinite	infinite">
+        </div>
+        <div class="user-info">
+            <h2>${user.name}</h2>
+            <p>${user.bio}</p>
+      
+        <ul class="info">
+            <li>${user.followers}<strong>Followers</strong></li>
+            <li>${user.following}<strong>Following</strong></li>
+            <li>${user.public_repos}<strong>Public Repos</strong></li>
+        </ul>
+        <div id="repos">
+        <h4>User's Public Repos :</h4>
+            </div>
+        </div>
+    </div>
+    `
+    document.getElementById("main").innerHTML = cardHTML
+}
+
+const addReposToCard = (repos) => {
+    const reposEl = document.getElementById("repos");
+    repos.slice(0,10).forEach(repo => {
+        const repoEl = document.createElement("a")
+        repoEl.classList.add("repo")
+        repoEl.href = repo.html_url;
+        repoEl.target = "_blank"
+        repoEl.innerText = repo.name
+
+        reposEl.append(repoEl);
+    });
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const user = search.value;
+
+    if (user) {
+        getUser(user);
+
+        search.value = "";
+    }
+});
